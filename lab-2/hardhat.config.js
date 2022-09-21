ade2a8bdfd3dcd149b500ae439c87f33ac1b7d71
roylai-infocorp <roy.lai@infocorp.io>
2022-09-21 17:00:23 +0800

    added lab-2

--- lab-2/hardhat.config.js
+++ lab-2/hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
 module.exports = {
   networks: {
     hardhat: {
       accounts: {
-        mnemonic: "--REPLACE-WITH-METAMASK-MNEMONIC-PASSPHRASE--",
+        mnemonic: process.env.GOERLI_MNEMONIC,
       },
     },
   },
