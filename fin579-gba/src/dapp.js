1db9d8fd452d2b953476ca131821bf8f71328847
RoyLai-InfoCorp <roy.lai@infocorp.io>
2022-10-12 11:51:58 +0800

    updated GBA contract addresses for goerli

--- fin579-gba/src/dapp.js
+++ fin579-gba/src/dapp.js
@@ -1,56 +1,70 @@
 import { ethers, BigNumber } from "ethers";
-const DISTILLERY_ADDRESS = "0x22902DBbB26fAF3fAfB76dfc0Ceb2d6Fb0EEd0a9";
-const CHRONIUM_ADDRESS = "0xA9cc515ab245df0F0B6Fd6d270A18679C99474ff";
+const CHRONIUM_ADDRESS = "0xDd6010412b61570bd6f0101460bb80bDbE103E28";
+const DISTILLERY_ADDRESS = "0xa20f2c420f14418b580f60A4964727B66f34C88d";
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 const tokens = [
-    {symbol:'AAAA',tokenAddress: "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
-    {symbol:'BBBB',tokenAddress: "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"},
-]
+  {
+    symbol: "AAAA",
+    tokenAddress: "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  },
+  {
+    symbol: "BBBB",
+    tokenAddress: "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+  },
+];
 
 const getAccount = async () => {
-    await provider.send("eth_requestAccounts", []); // Login to metamask
-    const account = provider.getSigner();
-    return account;
+  await provider.send("eth_requestAccounts", []); // Login to metamask
+  const account = provider.getSigner();
+  return account;
 };
 
-const distill = async(time)=>{
-    const account = await getAccount();
-    const distillery = new ethers.Contract(DISTILLERY_ADDRESS,["function distill(uint256)"],account);
-    const response = await distillery.distill(time-1);
-    const receipt = await response.wait();
-    console.log(receipt);
-}
+const distill = async (time) => {
+  const account = await getAccount();
+  const distillery = new ethers.Contract(
+    DISTILLERY_ADDRESS,
+    ["function distill(uint256)"],
+    account
+  );
+  const response = await distillery.distill(time - 1);
+  const receipt = await response.wait();
+  console.log(receipt);
+};
 
-const getTimeBalance = async()=>{
-    const account = await getAccount();
-    const chronium = new ethers.Contract(CHRONIUM_ADDRESS,["function checkTimeBalance(address) view returns(uint256)"],account);
-    return await chronium.checkTimeBalance(await account.getAddress());
-}
+const getTimeBalance = async () => {
+  const account = await getAccount();
+  const chronium = new ethers.Contract(
+    CHRONIUM_ADDRESS,
+    ["function checkTimeBalance(address) view returns(uint256)"],
+    account
+  );
+  return await chronium.checkTimeBalance(await account.getAddress());
+};
 
 // Get balance of ERC20 tokens
-const balanceOf = async(tokenAddr)=>{
-    // TBD
-    return 0
-}
+const balanceOf = async (tokenAddr) => {
+  // TBD
+  return 0;
+};
 
 // Sell exact amount of input tokens for output tokens using UniswapV2
 const sellTokens = async (inputAmt, inputAddr, outputAddr) => {
-    // TBD
+  // TBD
 };
 
 // Buy exact amount of output tokens for input tokens using UniswapV2
 const buyTokens = async (outputAmt, outputAddr, inputAddr) => {
-    // TBD
+  // TBD
 };
 
 export {
-    tokens,
-    distill,
-    getTimeBalance,
-    getAccount,
-    sellTokens,
-    buyTokens,
-    balanceOf,
-    CHRONIUM_ADDRESS
+  tokens,
+  distill,
+  getTimeBalance,
+  getAccount,
+  sellTokens,
+  buyTokens,
+  balanceOf,
+  CHRONIUM_ADDRESS,
 };
